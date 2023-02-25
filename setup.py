2428fa38b9cbd6941ddf71a1f2699b13c65d0d94
bookla <booklailert@gmail.com>
2023-02-25 22:28:11 +0000

    Update name

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ with open("README.md", 'r') as f:
 
 setup(
    name='APIGatewayHelper',
-   version='1.0.0a',
+   version='1.0.0b',
    description='Code for extracting arguments from POST requests to API Gateway and for returning values through '
                'API Gateway',
    long_description=long_description,
    author='Pakkapol Lailert',
    author_email='booklailert@gmail.com',
-   packages=['APIGateway'],
+   packages=['APIGatewayInterface'],
    install_requires=[],
 )
