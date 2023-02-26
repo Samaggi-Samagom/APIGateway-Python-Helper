1793730e104759b9311f47a25ddaaaf0cd96f9aa
bookla <booklailert@gmail.com>
2023-02-26 13:00:16 +0000

    Fixes to tests

--- APIGatewayInterface/Tests.py
+++ APIGatewayInterface/Tests.py
@@ -2,7 +2,7 @@ import json
 from typing import Dict, Any, Tuple
 
 
-def arguments_from_dict(data: Dict[str, Any]) -> Tuple[str, str]:
-    return json.dumps({
-        "body": data
-    }), ""
+def arguments_from_dict(data: Dict[str, Any]) -> Tuple[Dict[str, Any], str]:
+    return {
+        "body": json.dumps(data)
+    }, ""
