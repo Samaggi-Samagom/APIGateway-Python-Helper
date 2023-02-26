c00cda338ce053f78f54b732bf01d15c6a72cda8
bookla <booklailert@gmail.com>
2023-02-26 13:35:26 +0000

    Fixes

--- APIGatewayInterface/Tests.py
+++ APIGatewayInterface/Tests.py
@@ -4,9 +4,8 @@ from typing import Dict, Any, Callable
 
 class APIGatewayTestResponse:
 
-    def __init__(self, data: str):
-        data = json.loads(data)
-        self.body = data["body"]
+    def __init__(self, data: Dict[str, Any]):
+        self.body = json.loads(data["body"])
         self.data = self.body["data"] if "data" in self.body else None
         self.message = self.body["message"] if "message" in self.body else None
         self.status_code = data["statusCode"]
@@ -19,14 +18,14 @@ class APIGatewayTestResponse:
 
     def __str__(self):
         if self.status_code == 200:
-            return f"Function Returned HTTP {self.status_code} with message {self.message}.\nData: {self.data}."
+            return f"Function Returned HTTP {self.status_code} with message \"{self.message}\".\nData: {self.data}."
         else:
-            return f"Function Returned Error with HTTP code {self.status_code} and message {self.message}.\nThe " \
+            return f"Function Returned Error with HTTP code {self.status_code} and message \"{self.message}\".\nThe " \
                    f"reason for the error is {self.data['reason'] if 'reason' in self.data else 'not provided'}.\n" \
                    f"Error Data: {self.data['data']}"
 
 
-def post_to(func: Callable[[Dict[str, Any], Any], str], data: Dict[str, Any], context: Any = None) -> \
+def post_to(func: Callable[[Dict[str, Any], Any], Dict[str, Any]], data: Dict[str, Any], context: Any = None) -> \
         APIGatewayTestResponse:
     return APIGatewayTestResponse(func(event_from_dict(data), context))
 
