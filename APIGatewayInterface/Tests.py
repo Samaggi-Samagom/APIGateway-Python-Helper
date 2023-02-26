baa3dc76f11de9be34891c8f68cbe10dc7209808
bookla <booklailert@gmail.com>
2023-02-26 13:30:34 +0000

    Further improvements to data returned by test function

--- APIGatewayInterface/Tests.py
+++ APIGatewayInterface/Tests.py
@@ -5,7 +5,9 @@ from typing import Dict, Any, Callable
 class APIGatewayTestResponse:
 
     def __init__(self, data):
-        self.data = data["body"]
+        self.body = data["body"]
+        self.data = self.body["data"] if "data" in self.body else None
+        self.message = self.body["message"] if "message" in self.body else None
         self.status_code = data["statusCode"]
         self.allow_cors = \
             data["headers"] == {
@@ -13,6 +15,14 @@ class APIGatewayTestResponse:
                 'Access-Control-Allow-Methods': '*',
                 'Access-Control-Allow-Credentials': True
             } if "headers" in data else False
+
+    def __str__(self):
+        if self.status_code == 200:
+            return f"Function Returned HTTP {self.status_code} with message {self.message}.\nData: {self.data}."
+        else:
+            return f"Function Returned Error with HTTP code {self.status_code} and message {self.message}.\nThe " \
+                   f"reason for the error is {self.data['reason'] if 'reason' in self.data else 'not provided'}.\n" \
+                   f"Error Data: {self.data['data']}"
 
 
 def post_to(func: Callable[[Dict[str, Any], Any], Dict[str, Any]], data: Dict[str, Any], context: Any = None) -> \
