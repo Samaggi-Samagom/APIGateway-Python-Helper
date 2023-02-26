089d81957be9dce215169f66c984dff5f063c9ac
bookla <booklailert@gmail.com>
2023-02-26 13:24:19 +0000

    Improvements to data returned by test function

--- APIGatewayInterface/Tests.py
+++ APIGatewayInterface/Tests.py
@@ -1,9 +1,23 @@
 import json
-from typing import Dict, Any, Tuple, Callable
+from typing import Dict, Any, Callable
 
 
-def post_to(func: Callable[[Dict[str, Any], Any], Dict[str, Any]], data: Dict[str, Any], context: Any = None):
-    return func(event_from_dict(data), context)
+class APIGatewayTestResponse:
+
+    def __init__(self, data):
+        self.data = data["body"]
+        self.status_code = data["statusCode"]
+        self.allow_cors = \
+            data["headers"] == {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': '*',
+                'Access-Control-Allow-Credentials': True
+            } if "headers" in data else False
+
+
+def post_to(func: Callable[[Dict[str, Any], Any], Dict[str, Any]], data: Dict[str, Any], context: Any = None) -> \
+        APIGatewayTestResponse:
+    return APIGatewayTestResponse(func(event_from_dict(data), context))
 
 
 def event_from_dict(data: Dict[str, Any]) -> Dict[str, Any]:
