21544d873b94d852d43235bf592f09924c007f9a
bookla <booklailert@gmail.com>
2023-02-26 13:49:28 +0000

    Improvements to printing in tests

--- APIGatewayInterface/Tests.py
+++ APIGatewayInterface/Tests.py
@@ -4,7 +4,8 @@ from typing import Dict, Any, Callable
 
 class APIGatewayTestResponse:
 
-    def __init__(self, data: Dict[str, Any]):
+    def __init__(self, data: Dict[str, Any], function_name: str):
+        self.function_name = function_name
         self.body = json.loads(data["body"])
         self.data = self.body["data"] if "data" in self.body else None
         self.message = self.body["message"] if "message" in self.body else None
@@ -18,14 +19,16 @@ class APIGatewayTestResponse:
 
     def __str__(self):
         if self.status_code == 200:
-            return f"\n\n ------ Result ------\n\n" \
+            return f"\n\n ------ Result from {self.function_name} ------\n\n" \
                    f"Function Returned HTTP {self.status_code} with message \"{self.message}\".\n" \
-                   f"Data: {self.__pretty(self.data)}."
+                   f"Data: {self.__pretty(self.data)}." \
+                   f"------ End of Result ------ \n\n"
         else:
-            return f"\n\n ------ Result ------\n\n" \
+            return f"\n\n ------ Result from {self.function_name} ------\n\n" \
                 f"Function Returned Error with HTTP code {self.status_code} and message \"{self.message}\".\nThe " \
                    f"reason for the error is {self.data['reason'] if 'reason' in self.data else 'not provided'}.\n" \
-                   f"Error Data: {self.__pretty(self.data['data'])}"
+                   f"Error Data: {self.__pretty(self.data['data'])}\n\n" \
+                   f"------ End of Result ------ \n\n"
 
     @staticmethod
     def __pretty(d: Dict[str, Any]) -> str:
@@ -34,7 +37,7 @@ class APIGatewayTestResponse:
 
 def post_to(func: Callable[[Dict[str, Any], Any], Dict[str, Any]], data: Dict[str, Any], context: Any = None) -> \
         APIGatewayTestResponse:
-    return APIGatewayTestResponse(func(event_from_dict(data), context))
+    return APIGatewayTestResponse(func(event_from_dict(data), context), func.__name__)
 
 
 def event_from_dict(data: Dict[str, Any]) -> Dict[str, Any]:
