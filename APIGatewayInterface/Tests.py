3f123c6bd80e97b2d39a3d11152d9165472837a2
bookla <booklailert@gmail.com>
2023-02-26 13:18:33 +0000

    Improvements to how tests run functions

--- APIGatewayInterface/Tests.py
+++ APIGatewayInterface/Tests.py
@@ -1,8 +1,12 @@
 import json
-from typing import Dict, Any, Tuple
+from typing import Dict, Any, Tuple, Callable
 
 
-def arguments_from_dict(data: Dict[str, Any]) -> Tuple[Dict[str, Any], str]:
+def post_to(func: Callable[[Dict[str, Any], Any], Dict[str, Any]], data: Dict[str, Any], context: Any = None):
+    return func(event_from_dict(data), context)
+
+
+def event_from_dict(data: Dict[str, Any]) -> Dict[str, Any]:
     return {
         "body": json.dumps(data)
-    }, ""
+    }
