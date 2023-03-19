2ae85cab7a566f18dbab02337128269554709249
bookla <booklailert@gmail.com>
2023-03-19 15:14:36 +0000

    - Strict access now applies to nested members of required keys as well

--- APIGatewayInterface/Arguments.py
+++ APIGatewayInterface/Arguments.py
@@ -1,7 +1,31 @@
 from __future__ import annotations
+
+import copy
+
 from APIGatewayInterface.Responses import MissingArguments, BadRequest, response
 from typing import Dict, List, Any
 import json
+
+
+class ArgumentsDict(dict):
+
+    def __init__(self, x: dict, req: Dict[str, dict | str | None], enforce_access: bool):
+        super().__init__(x)
+        self._raw = copy.deepcopy(x)
+        self._req = req
+        self._enforce_access = enforce_access
+
+    def __getitem__(self, item):
+        if self._enforce_access and item not in self._req.keys():
+            raise KeyError(f"Trying to access \"{item}\" which is nested within a required member but not required.")
+
+        if item not in self._raw:
+            raise KeyError(f"Key \"{item}\" does not exist in dictionary.")
+
+        if type(self._raw[item]) == dict:
+            return ArgumentsDict(self._raw[item], self._req[item], self._enforce_access)
+        else:
+            return self._raw[item]
 
 
 class Arguments:
@@ -61,11 +85,17 @@ class Arguments:
         contains = []
         for key, inner_key in keys.items():
             if inner_key is None:
+                if type(in_dict) != dict:
+                    return False
                 contains.append(key in in_dict.keys())
             else:
                 if type(inner_key) is list:
+                    if type(in_dict[key] != dict):
+                        return False
                     contains.append(key in in_dict.keys() and all(x in in_dict[key].keys() for x in inner_key))
                 else:
+                    if key not in in_dict:
+                        return False
                     contains.append(key in in_dict.keys() and cls.__has_keys(in_dict[key], inner_key))
         return all(contains)
 
@@ -92,24 +122,27 @@ class Arguments:
             res.append({k: v})
         return res
 
-    @staticmethod
-    def __extract_keys(x: str | dict | list):
+    @classmethod
+    def __extract_keys(cls, x: str | dict | list):
+        # print(x)
         if type(x) is str:
             return {x: None}
         elif type(x) is dict:
-            return x
+            as_list = []
+            for d in Arguments.__separate_dict(x):
+                s = list(d.items())[0]
+                as_list.append((s[0], cls.__extract_keys(s[1])))
+            return dict(as_list)
         elif type(x) is list:
-            l = []
+            as_list = []
             for e in x:
                 if type(e) == dict:
-                    if len(e) < 1:
-                        continue
                     for d in Arguments.__separate_dict(e):
                         s = list(d.items())[0]
-                        l.append((s[0], s[1]))
+                        as_list.append((s[0], cls.__extract_keys(s[1])))
                 else:
-                    l.append((e, None))
-            return dict(l)
+                    as_list.append((e, None))
+            return dict(as_list)
         else:
             raise TypeError(f"Unable to extract requirements with type {type(x)}")
 
@@ -139,7 +172,11 @@ class Arguments:
                                "`.should_error()` before accessing first argument.")
         if self.__enforce_access and (item not in self._required_args.keys() and item not in self._optional_args):
             raise KeyError(f"Trying to access \"{item}\" which is not required nor optional.")
-        return self._arguments[item]
+
+        if type(self._arguments[item]) == dict:
+            return ArgumentsDict(self._arguments[item], self._required_args[item], self.__enforce_access)
+        else:
+            return self._arguments[item]
 
     def get(self, item):
         return self[item]
